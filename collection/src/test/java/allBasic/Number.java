6f5e13d7d60c107c5811b5360d1188f74ca5200a
asmatalukder3134 <asma.talukder@yahoo.com>
2020-06-15 11:12:00 -0400

    comitted

---
+++ collection/src/test/java/allBasic/Number.java
@@ -0,0 +1,20 @@
+package allBasic;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.testng.annotations.Test;
+
+public class Number {
+	String num;
+	String num1;
+	String num2;
+	String num3;
+
+	String random() {
+		String num=RandomStringUtils.randomAlphabetic(8);
+		String num1=RandomStringUtils.randomAlphabetic(10);
+		String num2=RandomStringUtils.randomAlphabetic(5);
+		String num3=RandomStringUtils.randomAlphabetic(7);
+		return num;
+	
+
+}}
