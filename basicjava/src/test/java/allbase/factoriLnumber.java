21169fe1cd8ce31f5727ab7c500d86ab796caee0
asmatalukder3134 <asma.talukder@yahoo.com>
2020-06-15 11:00:04 -0400

    nancy

---
+++ basicjava/src/test/java/allbase/factoriLnumber.java
@@ -0,0 +1,16 @@
+package allbase;
+
+public class factoriLnumber {
+
+	public static void main(String[] args) {
+    int fact=1;	
+	int a=5;
+		for(int i=1;i<=a;i++) {
+			 fact=fact*i;
+		}
+			
+		System.out.println("factorial num is:"+" "+fact);
+
+	}
+
+}
